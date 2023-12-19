ab9ea6ebf1a4d9c5964b51fb584b46d71b555c5a
gutehall <mathias@dm3.io>
2023-12-19 09:05:03 +0100

    Updated home.nix file

--- configuration.nix
+++ configuration.nix
@@ -1,33 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
+  networking.hostName = "nixos";
+  networking.wireless.enable = true;
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -42,10 +29,7 @@
     LC_TIME = "sv_SE.UTF-8";
   };
 
-  # Enable the X11 windowing system.
   services.xserver.enable = true;
-
-  # Enable the XFCE Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   
@@ -53,19 +37,19 @@
   gnome-photos
   gnome-tour
 ]) ++ (with pkgs.gnome; [
-  cheese # webcam tool
+  cheese
   gnome-music
   gnome-terminal
-  gedit # text editor
-  epiphany # web browser
-  geary # email reader
-  evince # document viewer
+  gedit
+  epiphany 
+  geary 
+  evince 
   gnome-characters
-  totem # video player
-  tali # poker game
-  iagno # go game
-  hitori # sudoku game
-  atomix # puzzle game
+  totem 
+  tali 
+  iagno 
+  hitori 
+  atomix 
 ]);
 
 
@@ -90,81 +74,57 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    config = {
+      General = {
+        Enable = "Source,Sink,Media,Socket";
+      };
+    };
+  };
+  services.blueman.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.mathias = {
-    isNormalUser = true;
-    description = "mathias";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      firefox
-      google-chrome
-    #  thunderbird
-    ];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim 
-    wget
-    #pkgs.virtualbox
-    pkgs.vagrant
-    pkgs.packer
-    pkgs.vscode
-    pkgs.slack
-    pkgs.git
-    pkgs.gh
-    pkgs.google-chrome
-    pkgs.zsh
-    pkgs.wgnord
-    pkgs.wireguard-tools
-    pkgs.openresolv
-    pkgs.gnomeExtensions.gnordvpn-local
-    pkgs.wireguard-go
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
+  # users.users.mathias = {
+  #   isNormalUser = true;
+  #   description = "mathias";
+  #   extraGroups = [ "networkmanager" "wheel" ];
+  #   packages = with pkgs; [
+  #     firefox
+  #     google-chrome
+  #   #  thunderbird
+  #   ];
   # };
 
-  # List services that you want to enable:
+  nixpkgs.config.allowUnfree = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  # environment.systemPackages = with pkgs; [
+  #   vim 
+  #   wget
+  #   #pkgs.virtualbox
+  #   pkgs.vagrant
+  #   pkgs.packer
+  #   pkgs.vscode
+  #   pkgs.slack
+  #   pkgs.git
+  #   pkgs.gh
+  #   pkgs.google-chrome
+  #   pkgs.zsh
+  #   pkgs.wgnord
+  #   pkgs.wireguard-tools
+  #   pkgs.openresolv
+  #   pkgs.gnomeExtensions.gnordvpn-local
+  #   pkgs.wireguard-go
+  # ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
-  hardware.bluetooth.enable = true;
+  system.stateVersion = "23.11";
   hardware.bluetooth.powerOnBoot = true;
   virtualisation.virtualbox.host.enable = true;
   virtualisation.virtualbox.guest.enable = true;
+
+  powerManagement.enable = true; 
 }
--- home.nix
+++ home.nix
@@ -1,19 +1,71 @@
 { config, pkgs, ... }:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
-{
-  imports = [
-    (import "${home-manager}/nixos")
+
+rec {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "mathias";
+  home.homeDirectory = "/home/mathisa";
+  home.packages = with pkgs; [
+    #pkgs.virtualbox
+    vagrant
+    packer
+    vscode
+    slack
+    git
+    gh
+    google-chrome
+    zsh
+    wgnord
+    wireguard-tools
+    openresolv
+    gnomeExtensions.gnordvpn-local
+    wireguard-go
+	unzip
+	wget
+	gnupg
+	htop
+	btop
+	
   ];
 
-  home-manager.users.my_username = {
-    /* The home.stateVersion option does not have a default and must be set */
-    home.stateVersion = "18.09";
-    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
- 
+  programs.git = {
+    enable = true;
+    userName = "gutehall";
+    userEmail = "mathias@dm3.io";
+  };
 
-# home.file = {
-# 	".zshrc".source		= ./sources/zshrc";
-# 	".config/gh/config.yaml	= ./sources/config.yaml";
-# };
+    #settings = { ignorecase = true; };
+    extraConfig = ''
+      set mouse=a
+      set ignorecase
+      set pastetoggle=<F2>
+      set clipboard+=unnamedplus
+    '';
+  };
+  xdg.mimeApps.defaultApplications = { "text/html" = ["chromium-browser.desktop"];};
+  programs.tmux.enable = true;
+  programs.tmux.keyMode = "vi";
+  programs.tmux.sensibleOnTop = true;
+  programs.tmux.shortcut = "a";
+  programs.tmux.terminal = "screen-256color";
+  programs.tmux.secureSocket = false;
+  programs.tmux.extraConfig = ''
+
+  # Make mouse useful in copy mode
+  setw -g mouse on
+  set-option -s set-clipboard off
+  bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "xclip -se c -i"
+  '';
+
+  programs.bash.enable = true;
+  programs.bash.initExtra = ''
+    set -o vi
+    alias grep="grep --color=auto"
+    # https://github.com/rycee/home-manager/pull/797#issuecomment-538464993
+    source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+    '';
+  };
+}

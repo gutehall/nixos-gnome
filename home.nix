ec66f0b64f056354b708210dade6e7c98a7811f7
gutehall <mathias@dm3.io>
2023-12-19 08:12:17 +0100

    Minor changes

--- home.nix
+++ home.nix
@@ -1,4 +1,19 @@
-home.file = {
-	".zshrc".source		= ./sources/zshrc";
-	".config/gh/config.yaml	= ./sources/config.yaml";
-};
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  home-manager.users.my_username = {
+    /* The home.stateVersion option does not have a default and must be set */
+    home.stateVersion = "18.09";
+    /* Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ]; */
+ 
+
+# home.file = {
+# 	".zshrc".source		= ./sources/zshrc";
+# 	".config/gh/config.yaml	= ./sources/config.yaml";
+# };

495eb6f65590ab069c4adcef161c9364d6eeaa74
Mathias Gutehall <mathias.gutehall@nordcloud.com>
2023-12-19 16:24:47 +0100

    Added more apps

--- home.nix
+++ home.nix
@@ -18,11 +18,6 @@ rec {
     google-chrome
     zsh
     blackbox-terminal
-    wgnord
-    wireguard-tools
-    openresolv
-    gnomeExtensions.gnordvpn-local
-    wireguard-go
     unzip
     wget
     gnupg
@@ -35,6 +30,9 @@ rec {
     kubectl
     minikube
     lazygit
+    todoist
+    spotify
+    teams-for-linux
     cmake
     libgit2
     rustup
@@ -43,7 +41,7 @@ rec {
   home.file = {
     ".zshrc".source = "./sources/zshrc";
   };
-  
+
   programs.zsh.enable = true;
 
   programs.git = {

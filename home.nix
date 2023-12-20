b3ccadebc574b741a966602605fcfc121252be9a
Mathias Gutehall <mathias@dm3.io>
2023-12-20 12:53:11 +0100

    Minor changes

--- home.nix
+++ home.nix
@@ -13,11 +13,7 @@ in
   home.homeDirectory = "/home/mathias";
   home.stateVersion = "23.11";
 
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
-  };
+  nixpkgs.config.allowUnfree = true;
 
   home.file = {
     "./oh-my-zsh/themes/bullet-train.zsh-theme".source = ./sources/bullet-train.zsh-theme;
